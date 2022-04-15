05e47cae360cc9c6c152c09bbc505f6a9874bad5
Josh Brown <joshuaebrown021@gmail.com>
2022-04-15 17:50:45 -0400

    Adding places controller

---
+++ controllers/places.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+
+
+// Get All -GET-
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+// Get One -GET-
+
+
+// Creating One -POST-
+
+
+// Update One -PUT-
+
+
+// Deleteing One -DELETE-
+
+
+module.exports = router
+
+// use req.params.id to get access to whats passed in after the first /
